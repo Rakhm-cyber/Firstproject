c000006ba3a32a98cb43759a8d46be62aab47dd1
Your Name <you@example.com>
2024-02-01 22:33:53 +0300

    New commit

--- БОТ.py.py
+++ БОТ.py.py
@@ -1,21 +1,25 @@
 import telebot
 import csv
-
-bot = telebot.TeleBot('6719205536:AAERrWNZMhCoCIwJAaFrhQV4SdQ3CdM1MKw')
-
-with open("Расписание.csv", encoding='utf-8') as r_file:
+from dotenv import load_dotenv
+import os
+load_dotenv()
+bot = telebot.TeleBot(os.getenv('TOKEN'))
+schedule = {}
+with open("Расписание.csv", encoding='utf-8-sig') as r_file:
     file_reader = csv.reader(r_file, delimiter=";")
     for row in file_reader:
         command = row[0]
-        message_text = f"{row[1]}\n{row[2]}"
+        message_text = f"{row[2]}\n{row[1]}"
+        if schedule.get(command, None):
+            schedule[command] = schedule[command] + "\n" + message_text
+        else:
+            schedule[command] = message_text
 
-        @bot.message_handler(commands=[command])
-        def handle_message(message):
-            bot.send_message(message.chat.id, message_text)
-
+@bot.message_handler(commands=["Вторник"])
+def handle_message(message):
+    bot.send_message(message.chat.id, schedule["Вторник"])
+@bot.message_handler(commands=["Понедельник"])
+def handle_message(message):
+    bot.send_message(message.chat.id, schedule["Понедельник"])
 # Запуск бота
 bot.polling(none_stop=True)
-
-
-
-
